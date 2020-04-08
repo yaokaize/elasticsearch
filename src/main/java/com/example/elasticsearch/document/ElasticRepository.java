2f85b09b83cd9c55d56a0878c3b1ae61f67a1e4e
Kaiser <zz_ykz@163.com>
2020-04-08 09:14:45 +0800

    first commit

---
+++ src/main/java/com/example/elasticsearch/document/ElasticRepository.java
@@ -0,0 +1,15 @@
+package com.example.elasticsearch.document;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+
+public interface ElasticRepository extends ElasticsearchRepository<DocBean, Long> {
+
+    Page<DocBean> findByContent(String content, Pageable pageable);
+
+    Page<DocBean> findByFirstCode(String firstCode, Pageable pageable);
+
+    Page<DocBean> findBySecondCode(String secondCode, Pageable pageables);
+}
